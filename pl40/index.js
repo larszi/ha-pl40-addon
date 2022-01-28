b7f096515b6a162ad4acb3a6e3beadaa526149b9
joikel <r3lize@web.de>
2022-01-28 14:08:54 +0100

    Update Config

--- pl40/index.js
+++ pl40/index.js
@@ -1,2 +1,7 @@
 const test = 8000;
 console.log('Init.')
+
+var username = process.env.USERNAME
+var password = process.env.PASSWORD
+console.log('Username. %s', username)
+console.log('password. %s', password)
