6d07d3c9186d2a1c908406302bab540d4cc46389
Lars Zimmermann <info@zimmermann-lars.de>
2022-04-03 15:59:39 +0200

    Added ENV

--- pl40/run.sh
+++ pl40/run.sh
@@ -7,5 +7,17 @@ bashio::log.info "Username configured as ${USERNAME}."
 export PASSWORD=$(bashio::config 'password')
 bashio::log.info "Password configured as ${PASSWORD}."
 
+export DASHBOARD=$(bashio::config 'dashboard')
+bashio::log.info "Dashboard configured as ${DASHBOARD}."
+
+export MQTTUSER=$(bashio::config 'mqttuser')
+bashio::log.info "MQTT User configured as ${MQTTUSER}."
+
+export MQTTPW=$(bashio::config 'mqttpw')
+bashio::log.info "MQTT PW configured as ${MQTTPW}."
+
+export MQTTURL=$(bashio::config 'mqtturl')
+bashio::log.info "MQTT url configured as ${MQTTURL}."
+
 bashio::log.info "Starting bridge service."
 npm run start
