f0b10997ddcbc79463b9aa33c27f125d69f198f4
larszi <info@zimmermann-lars.de>
2023-03-13 13:46:26 +0100

    Added extra error

--- pl40/index.js
+++ pl40/index.js
@@ -32,7 +32,7 @@ const options = {
   username: mqtt_username,
   password: mqtt_password,
 }
-//Change this diry shit later! (Reload token after 24h) WatchDog will restart  
+//Change this diry shit later! (Reload token after 24h) WatchDog will restart
 function exitPlugin() {
   console.log("Exit Plugin");
   process.exit(1);
@@ -67,37 +67,38 @@ function mainWS() {
   axios.post('https://www.plexlog.de/login/', form, {
     headers: form.getHeaders(),
   }).then(result => {
-    
+
     cookie = result.headers['set-cookie']
     console.log(cookie[0].split('; ')[0]);
-  
+
     ws = new WebSocket('wss://live.plexlog.de/websockets/', {
       headers: {
         Cookie: cookie
        },
       });
-      
+
     ws.on('open', function open() {
       ws.send(dashboard_id);
       ws.send('--heartbeat--');
       setInterval(sendHeartbeat, 2500000, ws)
     });
-  
+
     ws.on('error', function errorFunc(err){
       console.log('websocket error')
       console.log(err)
+      exit(1);
       mainWS();
     })
-  
+
     function sendHeartbeat(w) {
       w.send('--heartbeat--');
     }
 
-  
-  
+
+
     ws.on('message', function message(data) {
       var dataraw = data;
-  
+
       var BatterieStand;
       var CurrentPowerSolar;
       var CurrentUsage;
@@ -108,13 +109,13 @@ function mainWS() {
       CurrentUsage = dataraw.toString().split(';')[64]
       CurrentUsageFromNetwork = dataraw.toString().split(';')[63]
       CurrentBatterieLoadingAmount = dataraw.toString().split(';')[65]
-  
+
       client.publish('solar/BatterieStand', BatterieStand)
       client.publish('solar/CurrentPowerSolar', CurrentPowerSolar.toString())
       client.publish('solar/CurrentUsage', CurrentUsage)
       client.publish('solar/CurrentUsageFromNetwork', CurrentUsageFromNetwork)
       client.publish('solar/CurrentBatterieLoadingAmount', CurrentBatterieLoadingAmount)
-  
+
     });
   });
 
